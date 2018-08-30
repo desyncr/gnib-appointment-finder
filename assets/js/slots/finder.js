ed493895dc379a469be5be671121fc73321274bf
mariodes@ <mariodes@amazon.com>
2018-08-30 22:28:07 +0100

    fix response parser: stop double-parsing JSON

--- assets/js/slots/finder.js
+++ assets/js/slots/finder.js
@@ -3,8 +3,7 @@ class SlotsFinder {
     return new Promise((resolve, reject) => {
       fetch('https://burghquayregistrationoffice.inis.gov.ie/Website/AMSREG/AMSRegWeb.nsf/(getApps4DTAvailability)?openpage&&cat=Work&sbcat=All&typ=Renewal&_3123123')
         .then(response => response.json())
-        .then(json => JSON.parse(json.slots))
-        .then(slots => resolve(slots))
+        .then(json => resolve(json.slots))
         .catch(err => reject(err));
     });
   }
