68a863846f31ec800adde56bf53f89995f31839d
mariodes@ <mariodes@amazon.com>
2018-08-31 08:49:25 +0100

    fix body parser when slots is a string instead of an array

--- assets/js/slots/finder.js
+++ assets/js/slots/finder.js
@@ -3,7 +3,8 @@ class SlotsFinder {
     return new Promise((resolve, reject) => {
       fetch('https://burghquayregistrationoffice.inis.gov.ie/Website/AMSREG/AMSRegWeb.nsf/(getApps4DTAvailability)?openpage&&cat=Work&sbcat=All&typ=Renewal&_3123123')
         .then(response => response.json())
-        .then(json => resolve(json.slots))
+        .then(json => typeof json.slots == 'string' ? [] : json.slots)
+        .then(slots => resolve(slots))
         .catch(err => reject(err));
     });
   }
